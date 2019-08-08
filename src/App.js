9c696a10aa97f56dc76d35ad9d59ee81af9b1980
Alexey Yudin <alexey.y.yudin@gmail.com>
2019-08-08 21:37:01 +0200

    Router example.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {Component} from 'react';
 import './App.css';
 import {BookList} from './book-list/book-list';
+import {AboutPage} from './about-page/about-page';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <BookList/>
-      </div>
+      <Router>
+        <Link to='/about'>About</Link>
+
+        <div className="app">
+          <Route exact path="/" component={BookList}/>
+          <Route path="/about" component={AboutPage}/>
+          <Route/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/about-page/about-page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Component} from 'react';
+
+export class AboutPage extends Component {
+  render() {
+    return (
+      <div>
+        About page
+      </div>
+    );
+  }
+}
